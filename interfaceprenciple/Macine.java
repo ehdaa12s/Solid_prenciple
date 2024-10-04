59db033a2e42bda863e4ef959b318c114a959b97
Ehdaa Abdullah <112123984+ehdaa12s@users.noreply.github.com>
2024-10-04 22:49:32 +0300

    interface  prenciple

---
+++ interfaceprenciple/Macine.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaceprenciple;
+
+/**
+ *
+ * @author ehdaaabdallah
+ */
+public interface Macine {
+    void start();
+    void stop();
+    
+    
+}
---
+++ interfaceprenciple/PrinterMachine.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package interfaceprenciple;
+
+/**
+ *
+ * @author ehdaaabdallah
+ */
+public class PrinterMachine implements Macine{
+
+    @Override
+    public void start() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public void stop() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    public void print(){}
+}
